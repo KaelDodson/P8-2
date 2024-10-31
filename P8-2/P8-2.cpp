61a2061fe3184a65d46b9a2b6aabd5e455f744ff
Kael Dodson <k-dodson.2@onu.edu>
2024-10-31 11:42:31 -0400

    completed countWords function.

--- P8-2/P8-2.cpp
+++ P8-2/P8-2.cpp
@@ -1,3 +1,35 @@
 #include <iostream>
+#include <string>
 
-using namespacestd;
+using namespace std;
+
+int countWords(string str) {
+    int wordCount = 0;
+    bool inWord = false;
+
+    for (char character : str) {
+        if (character == ' ') {
+            inWord = false;
+        }
+        else {
+            if (!inWord) {
+                wordCount++;
+                inWord = true;
+            }
+        }
+    }
+
+    return wordCount;
+}
+
+int main(void) {
+    string testStr1 = "Wazzzzzzup brah";
+    string testStr2 = "I'm testing to see if dis shat works";
+    string testStr3 = "Does it??? lets find out brah";
+
+    cout << "Number of words in '" << testStr1 << "': " << countWords(testStr1) << endl;
+    cout << "Number of words in '" << testStr2 << "': " << countWords(testStr2) << endl;
+    cout << "Number of words in '" << testStr3 << "': " << countWords(testStr3) << endl;
+
+    return 0;
+}
